e99f1f08bf276d257ca65ef884b41a1ece118a69
PeytonS <peyton.sandridge@gmail.com>
2020-01-07 20:48:39 -0500

    removed unneccessary imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverController.java
@@ -1,15 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import java.util.Scanner;
 
 @TeleOp(name="Driver Controller")
 public class DriverController extends SuperOp {
 
 
-    private final float latch_cd = 1000;
+    private final float latch_cd = 500;
 
     private boolean trapdoorState = false;
 
@@ -70,122 +67,6 @@ public class DriverController extends SuperOp {
         RightStoneRamp.setPower(power);
         LeftStoneRamp.setPower(power);
     }
-
-
-    /*private void dropArm() {
-        switch (arm) {
-            case INIT:
-                // set target position
-                currPosArm = Flipper.getPosition();
-                targetPosArm = 1;
-                arm = ARMSTATE.DROPPING;
-                break;
-            case RAISED:
-                currPosArm = Flipper.getPosition();
-                targetPosArm = currPosArm += 0.5;
-                arm = ARMSTATE.DROPPING;
-                break;
-            case DROPPING:
-                if (currPosArm >= targetPosArm) {
-                    arm = ARMSTATE.DROPPED;
-                }
-                // want it to go up in increments so the program can take other inputs
-                currPosArm = Flipper.getPosition();
-                Flipper.setPosition(currPosArm + 0.1);
-                break;
-            // dont want to do anything if the arm is raising or dropping
-            case DROPPED:
-                break;
-            case RAISING:
-                break;
-        }
-    }
-
-    // find way to combine this with dropArm()
-    private void raiseArm() {
-        switch (arm) {
-            case INIT:
-                // set target position
-                currPosArm = Flipper.getPosition();
-                targetPosArm = 0;
-                arm = ARMSTATE.RAISING;
-                break;
-            case DROPPED:
-                currPosArm = Flipper.getPosition();
-                targetPosArm = currPosArm - 0.5;
-                arm = ARMSTATE.RAISING;
-                break;
-            case RAISING:
-                // want it to go up in increments so the program can take other inputs
-                if (currPosArm <= targetPosArm) {
-                    arm = ARMSTATE.RAISED;
-                }
-                currPosArm = Flipper.getPosition();
-                Flipper.setPosition(currPosArm - 0.1);
-
-                break;
-            // dont want to do anything if the arm is raising or dropping
-            case RAISED:
-                break;
-            case DROPPING:
-                break;
-        }
-    }
-*/
-
-/*    // NOT WORKING
-    private void openDoor() {
-        switch (door) {
-            case INIT:
-            case CLOSED:
-                // set target position
-                targetPosDoor = 0.95;
-                currPosDoor = Trapdoor.getPosition();
-                door = DOORSTATE.OPEN;
-                break;
-            case OPEN:
-                if (currPosDoor >= targetPosDoor) {
-                    door = DOORSTATE.OPENED;
-                }
-                // want it to go up in increments so the program can take other inputs
-                Trapdoor.setPosition(currPosDoor + 0.1);
-                currPosDoor += 0.1;
-                break;
-            // dont want to do anything if the arm is raising or dropping
-            case OPENED:
-                break;
-            case CLOSE:
-                break;
-        }
-    }
-    private void closeDoor() {
-        switch (door) {
-            case INIT:
-                // set target position
-                targetPosDoor = 0;
-                currPosDoor = Trapdoor.getPosition();
-                door = DOORSTATE.CLOSE;
-                break;
-            case OPENED:
-                targetPosDoor = 0;
-                currPosDoor = Trapdoor.getPosition();
-                door = DOORSTATE.CLOSE;
-                break;
-            case CLOSE:
-                if (currPosDoor <= targetPosDoor) {
-                    door = DOORSTATE.CLOSED;
-                }
-                // want it to go up in increments so the program can take other inputs
-                Trapdoor.setPosition(currPosDoor - 0.1);
-                currPosDoor -= 0.1;
-                break;
-            // dont want to do anything if the arm is raising or dropping
-            case CLOSED:
-                break;
-            case OPEN:
-                break;
-        }
-    }*/
 }
 
 
