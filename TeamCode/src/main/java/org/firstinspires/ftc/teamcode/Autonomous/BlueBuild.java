f350e74d9fcb6283373107e5892c88c53c63dbe6
Asher <ashkornfeld@gmail.com>
2020-01-10 17:04:47 -0500

    RedBuild works and autonomous goes through without hitches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueBuild.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueBuild.java
@@ -10,16 +10,14 @@ public class BlueBuild extends SuperOp {
 
     //This uses an enum declared in SuperOp
     //It declares the first STATUS as "START"
-    private STATUS status = STATUS.START;
+    private STATUS status = STATUS.FlIPPER;
 
     //create new stopwatch
     private ElapsedTime time = new ElapsedTime();
     private double targetTime;
     private ElapsedTime arm = new ElapsedTime();
     private int currPosition;
-    private int startPosition;
     private int targetPosition;
-    private int targetPosition1;
     private boolean ran = false;
     private boolean ran1 = true;
 
@@ -30,6 +28,7 @@ public class BlueBuild extends SuperOp {
         //this allows us to see how the motors are behaving in the code
         //and then compare it to how they perform in real life
         telemetry.addData("Arm", arm.seconds());
+        telemetry.addData("Power",LatchMotor.getPower());
         telemetry.addData("LatchMotor Position: ", LatchMotor.getCurrentPosition());
         telemetry.addData("Time: ", time.seconds());
         telemetry.addData("Front Right: ", FrontRightDrive.getCurrentPosition());
@@ -38,15 +37,15 @@ public class BlueBuild extends SuperOp {
         telemetry.addData("Front Left: ", FrontLeftDrive.getCurrentPosition());
         telemetry.addData("Status: ", status);
         telemetry.addData("Latch Position: ", Latch.getPosition());
-        telemetry.addData("Power",LatchMotor.getPower());
 
         currPosition = LatchMotor.getCurrentPosition();
-        startPosition = LatchMotor.getCurrentPosition();
-        targetPosition1 = startPosition;
         //switch statements for changing the status of the robot
         //this allows us to use different code for each status
         //there are methods created below the switch statement for easier reading
         switch (status) {
+            case FlIPPER:
+                flipper();
+                break;
              case START:
                 start1();
                 break;
@@ -67,6 +66,9 @@ public class BlueBuild extends SuperOp {
             case TOBUILD:
                 toBuild();
                 break;
+            case RELEASEBLOCK:
+                release();
+                break;
             case PARK:
                 //time.reset();
                 park();
@@ -80,12 +82,24 @@ public class BlueBuild extends SuperOp {
     //this is the first method run
     //it resets the elapsed time
     //then switches the status to 'TOBLOCK'
+    private void flipper(){
+        if(ran1){
+            time.reset();
+            ran1 = !ran1;
+        }
+        targetTime = 2;
+        Flipper.setPower(-1);
+        if(time.seconds() >= targetTime){
+            Flipper.setPower(0);
+            status = STATUS.START;
+        }
+    }
     private void start1(){
         if(ran == false){
             time.reset();
             ran = !ran;
         }
-        targetTime = 0.3;
+        targetTime = 0.5;
         drive(0.5,0,0);
         if(time.seconds() >= targetTime){
             drive(0,0,0);
@@ -102,7 +116,7 @@ public class BlueBuild extends SuperOp {
     private void toBlock() {
         //move forward for 3 seconds
         leftSpeedMultiplier = 1;
-        targetTime = 2.4;
+        targetTime = 2.5;
         drive(0, 0.5, 0);
 
         // vision code
@@ -126,7 +140,7 @@ public class BlueBuild extends SuperOp {
     //if time >= 1.5 seconds, the robot stops
     //and switches the STATUS to 'GETBLOCK'
     private void approach() {
-        targetTime = 1.3;
+        targetTime = 0.9;
         drive(0.5, 0, 0);
 
         if(time.seconds() >= targetTime) {
@@ -157,7 +171,7 @@ public class BlueBuild extends SuperOp {
             LatchMotor.setPower(0);
             //switches STATUS
             //resets clock
-            if(ran1){
+            if(!ran1){
                 time.reset();
                 ran1 = !ran1;
             }
@@ -192,17 +206,8 @@ public class BlueBuild extends SuperOp {
         drive(0,-0.5,0);
         if(time.seconds() >= targetTime) {
             drive(0, 0, 0);
-            if (!ran) {
-                arm.reset();
-                ran = !ran;
-            }
-            currPosition = LatchMotor.getCurrentPosition();
-            //rotate arm up
-            LatchMotor.setPower(-0.3);
-            drive(0,0,0);
-            LatchMotor.setPower(0);
-            time.reset();
-            status = STATUS.PARK;
+            arm.reset();
+            status = STATUS.RELEASEBLOCK;
             //check if the arm is in position
             /*if ((currPosition <= targetPosition + 13 && currPosition >= targetPosition - 6) || arm.seconds() > 1) { drive(0,0,0);
                 //leave motor
@@ -212,6 +217,17 @@ public class BlueBuild extends SuperOp {
             } */
         }
     }
+    private void release(){
+        currPosition = LatchMotor.getCurrentPosition();
+        //rotate arm up
+
+        LatchMotor.setPower(-0.3);
+        if(arm.seconds() >= 1) {
+            LatchMotor.setPower(0);
+            time.reset();
+            status = STATUS.PARK;
+        }
+    }
 
     //park the robot in the middle of the alliance bridge
     //drive 1.5 seconds forward, then stop
@@ -219,12 +235,13 @@ public class BlueBuild extends SuperOp {
     private void park() {
         // vision code to park the robot under the bridge
         //t_drive(0, -1, 0, 1);
-        targetTime = .8;
+        targetTime = .9;
         drive(0, 0.5, 0);
         if(time.seconds() >= targetTime){
             //stop robot
+            drive(0.5,0,0);
+            sleep_secs(0.3);
             drive(0,0,0);
-            sleep_secs(0.5);
             //switch STATUS
             status = STATUS.STOP;
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedBuild.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedBuild.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.SuperOp;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
@@ -8,12 +10,16 @@ public class RedBuild extends SuperOp {
 
     //This uses an enum declared in SuperOp
     //It declares the first STATUS as "START"
-    private STATUS status = STATUS.START;
-    boolean running = true;
-    private int targetPosition;
-    private int currPosition;
+    private STATUS status = STATUS.FlIPPER;
+
+    //create new stopwatch
     private ElapsedTime time = new ElapsedTime();
     private double targetTime;
+    private ElapsedTime arm = new ElapsedTime();
+    private int currPosition;
+    private int targetPosition;
+    private boolean ran = false;
+    private boolean ran1 = true;
 
     @Override
     public void loop() {
@@ -21,20 +27,25 @@ public class RedBuild extends SuperOp {
         //declare telemetry for all motors/servos
         //this allows us to see how the motors are behaving in the code
         //and then compare it to how they perform in real life
+        telemetry.addData("Arm", arm.seconds());
+        telemetry.addData("Power",LatchMotor.getPower());
         telemetry.addData("LatchMotor Position: ", LatchMotor.getCurrentPosition());
-        telemetry.addData("Time: ", time.milliseconds());
+        telemetry.addData("Time: ", time.seconds());
         telemetry.addData("Front Right: ", FrontRightDrive.getCurrentPosition());
         telemetry.addData("Back Left: ", BackLeftDrive.getCurrentPosition());
         telemetry.addData("Back Right: ", BackRightDrive.getCurrentPosition());
         telemetry.addData("Front Left: ", FrontLeftDrive.getCurrentPosition());
         telemetry.addData("Status: ", status);
         telemetry.addData("Latch Position: ", Latch.getPosition());
-        currPosition = LatchMotor.getCurrentPosition();
 
+        currPosition = LatchMotor.getCurrentPosition();
         //switch statements for changing the status of the robot
         //this allows us to use different code for each status
-        //there are methods created below the switch statments for easier reading
+        //there are methods created below the switch statement for easier reading
         switch (status) {
+            case FlIPPER:
+                flipper();
+                break;
             case START:
                 start1();
                 break;
@@ -46,14 +57,20 @@ public class RedBuild extends SuperOp {
                 break;
             case GETBLOCK:
                 getBlock();
+                //status = STATUS.AWAY;
                 break;
             case AWAY:
-                away();
+                //time.reset();
+                //away();
                 break;
             case TOBUILD:
                 toBuild();
                 break;
+            case RELEASEBLOCK:
+                release();
+                break;
             case PARK:
+                //time.reset();
                 park();
                 break;
             case STOP:
@@ -65,28 +82,56 @@ public class RedBuild extends SuperOp {
     //this is the first method run
     //it resets the elapsed time
     //then switches the status to 'TOBLOCK'
+    private void flipper(){
+        if(ran1){
+            time.reset();
+            ran1 = !ran1;
+        }
+        targetTime = 2;
+        Flipper.setPower(-1);
+        if(time.seconds() >= targetTime){
+            Flipper.setPower(0);
+            status = STATUS.START;
+        }
+    }
     private void start1(){
-        time.reset();
-        status = STATUS.TOBLOCK;
+        if(ran == false){
+            time.reset();
+            ran = !ran;
+        }
+        targetTime = 0.5;
+        drive(-0.5,0,0);
+        if(time.seconds() >= targetTime){
+            drive(0,0,0);
+            time.reset();
+            status = STATUS.TOBLOCK;
+            ran = false;
+        }
+        //targetTime = .5;
     }
 
     //method to go to block
     //moves forward for 3 seconds at a motor power of .5
     //if the time is >= 3 seconds, the STATUS changes to 'APPROACH'
     private void toBlock() {
-        targetTime = 3;
+        //move forward for 3 seconds
+        leftSpeedMultiplier = 1;
+        targetTime = 2.5;
         drive(0, -0.5, 0);
+
         // vision code
         // if skystone is sighted
-        // set movement values to go towards block
+        /*
+         */
 
-        if(time.seconds() >= targetTime) {
+        // set movement values to go towards block
+        if(time.seconds()-targetTime > 0) {
             //stop
             drive(0,0,0);
-            sleep_secs(0.5);
+            //sleep_secs(0.5);
             //switch STATUS
-            status = STATUS.APPROACH;
             time.reset();
+            status = STATUS.APPROACH;
         }
     }
 
@@ -95,17 +140,16 @@ public class RedBuild extends SuperOp {
     //if time >= 1.5 seconds, the robot stops
     //and switches the STATUS to 'GETBLOCK'
     private void approach() {
-        targetTime = 1.5;
-        drive(0.5, 0, 0);
-        telemetry.addData("Status: ", status);
+        targetTime = 0.9;
+        drive(-0.5, 0, 0);
 
         if(time.seconds() >= targetTime) {
             //robot stops for .5 secs
             drive(0,0,0);
-            sleep_secs(0.5);
+            //sleep_secs(0.5);
             //switches STATUS
+            //resets clock
             status = STATUS.GETBLOCK;
-            time.reset();
         }
     }
 
@@ -113,37 +157,35 @@ public class RedBuild extends SuperOp {
     //check to see if the arm is in position
     //pull the block in and switch STATUS to 'AWAY'
     private void getBlock() {
-        //rotate the arm down
+        if(!ran){
+            arm.reset();
+            ran = !ran;
+        }
         currPosition = LatchMotor.getCurrentPosition();
-        targetPosition = currPosition - 577;
-        LatchMotor.setPower(-0.7);
+        //rotate arm down
+        LatchMotor.setPower(0.3);
+        sleep_secs(.5);
+        if ((currPosition <= targetPosition + 13 && currPosition >= targetPosition - 6)|| arm.seconds() > 1) {
 
-        //check if the arm is in position
-        if (currPosition <= targetPosition + 13 && currPosition >= targetPosition - 6) {
             //pull the block in
             LatchMotor.setPower(0);
-            //switch STATUS
-            status = STATUS.AWAY;
-            time.reset();
-        }
-    }
-
-    //strafe left for 1.5 seconds
-    //then stop and switch STATUS to 'AWAY'
-    private void away() {
-        //strafe right
-        leftSpeedMultiplier = 1.5;
-        targetTime = 1.5;
-        drive(-0.5, 0, 0);
-        telemetry.addData("Status: ", status);
-
-        if(time.seconds() >= targetTime) {
-            //stop
-            drive(0,0,0);
-            sleep_secs(0.5);
-            //switch STATUS
-            status = STATUS.TOBUILD;
-            time.reset();
+            //switches STATUS
+            //resets clock
+            if(!ran1){
+                time.reset();
+                ran1 = !ran1;
+            }
+            targetTime = 1.1;
+            drive(0.5, 0, 0);
+            if(time.seconds() >= targetTime) {
+                //stop
+                drive(0,0,0);
+                //sleep_secs(0.5);
+                //switch STATUS
+                //resets clock
+                time.reset();
+                status = STATUS.TOBUILD;
+            }
         }
     }
 
@@ -153,27 +195,37 @@ public class RedBuild extends SuperOp {
     //check if the arm is up
     //lastly, set STATUS to 'PARK'
     private void toBuild() {
-        targetPosition = currPosition + 577;
-        // methods to get the robot back to the build site to place down the block
-        targetTime = 3;
-        drive(0,-0.5,0);
-        telemetry.addData("Status: ", status);
+
+        if(ran){
+            targetPosition = LatchMotor.getCurrentPosition() + 500;
+            ran = !ran;
+        }
+        //sets target position for grabber
+        //methods to get the robot back to the build site to place down the block
+        targetTime = 2;
+        drive(0,0.5,0);
         if(time.seconds() >= targetTime) {
             drive(0, 0, 0);
-            //rotate the arm up
-            currPosition = LatchMotor.getCurrentPosition();
-            LatchMotor.setPower(0.7);
-
+            arm.reset();
+            status = STATUS.RELEASEBLOCK;
             //check if the arm is in position
-            if (currPosition <= targetPosition + 13 && currPosition >= targetPosition - 13) {
-                //pull the block in
+            /*if ((currPosition <= targetPosition + 13 && currPosition >= targetPosition - 6) || arm.seconds() > 1) { drive(0,0,0);
+                //leave motor
                 LatchMotor.setPower(0);
-                Latch.setPosition(0);
-                //switch STATUS
-                status = STATUS.PARK;
                 time.reset();
-                leftSpeedMultiplier = 1;
-            }
+                status = STATUS.PARK;
+            } */
+        }
+    }
+    private void release(){
+        currPosition = LatchMotor.getCurrentPosition();
+        //rotate arm up
+
+        LatchMotor.setPower(-0.3);
+        if(arm.seconds() >= 1) {
+            LatchMotor.setPower(0);
+            time.reset();
+            status = STATUS.PARK;
         }
     }
 
@@ -181,15 +233,15 @@ public class RedBuild extends SuperOp {
     //drive 1.5 seconds forward, then stop
     //switch STATUS to 'STOP'
     private void park() {
-        //vision code to park the robot under the bridge
+        // vision code to park the robot under the bridge
         //t_drive(0, -1, 0, 1);
-        targetTime = 1.5;
-        drive(0, 0.5, 0);
-
+        targetTime = .9;
+        drive(0, -0.5, 0);
         if(time.seconds() >= targetTime){
             //stop robot
+            drive(-0.5,0,0);
+            sleep_secs(0.3);
             drive(0,0,0);
-            sleep_secs(0.5);
             //switch STATUS
             status = STATUS.STOP;
         }
@@ -199,8 +251,11 @@ public class RedBuild extends SuperOp {
     //set all motor powers to 0
     private void stop1(){
         drive(0,0,0);
+        LatchMotor.setPower(0);
+        Flipper.setPower(0);
     }
 }
+
 
 
 
@@ -224,5 +279,4 @@ SPONGEBOB SQUAREPANTS!
 SPONGEBOB SQUAREPANTS!
 SPONGEBOB SQUAREPANTS!
 SPONGEBOB SQUAREPANTS!
-
  */
