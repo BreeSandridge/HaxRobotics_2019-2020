d8cac3d1593ef90d23b9773b3b07b6df988e8598
Asher <ashkornfeld@gmail.com>
2020-01-14 21:05:58 -0500

    Continued working on autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OpModes/RedTriangle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OpModes/RedTriangle.java
@@ -2,37 +2,23 @@ package org.firstinspires.ftc.teamcode.Autonomous.OpModes;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.Autonomous.BuildSuperOp;
-import org.firstinspires.ftc.teamcode.SuperOp;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
 public class RedTriangle extends BuildSuperOp {
 
     //This uses an enum declared in SuperOp
     //It declares the first STATUS as "START"
-    private STATUS status = STATUS.FLIPPER;
-
+    private BUILDSTATUS status = BUILDSTATUS.FLIPPER;
     //create new stopwatch
-    private ElapsedTime time = new ElapsedTime();
-    private double targetTime;
-    private ElapsedTime arm = new ElapsedTime();
-    private int currPosition;
-    private int targetPosition;
-    private boolean ran = false;
-    private boolean ran1 = true;
 
     @Override
     public void loop() {
-        startPoint = -1;
+        startPointBuild = -1;
         //declare telemetry for all motors/servos
         //this allows us to see how the motors are behaving in the code
         //and then compare it to how they perform in real life
-        telemetry.addData("Arm", arm.seconds());
         telemetry.addData("Power", LatchMotor.getPower());
         telemetry.addData("LatchMotor Position: ", LatchMotor.getCurrentPosition());
-        telemetry.addData("Time: ", time.seconds());
         telemetry.addData("Front Right: ", FrontRightDrive.getCurrentPosition());
         telemetry.addData("Back Left: ", BackLeftDrive.getCurrentPosition());
         telemetry.addData("Back Right: ", BackRightDrive.getCurrentPosition());
@@ -40,40 +26,29 @@ public class RedTriangle extends BuildSuperOp {
         telemetry.addData("Status: ", status);
         telemetry.addData("Latch Position: ", Latch.getPosition());
 
-        currPosition = LatchMotor.getCurrentPosition();
         //switch statements for changing the status of the robot
         //this allows us to use different code for each status
         //there are methods created below the switch statement for easier reading
         switch (status) {
             case FLIPPER:
                 flipper();
+                status = BUILDSTATUS.TOFOUNDATION;
                 break;
-            case START:
-                start1();
+            case TOFOUNDATION:
+                toFoundation();
+                status = BUILDSTATUS.DRAG;
                 break;
-            case TOBLOCK:
-                toBlock();
+            case DRAG:
+                drag();
+                status = BUILDSTATUS.AROUND;
                 break;
-            case APPROACH:
-                approach();
-                break;
-            case GETBLOCK:
-                getBlock();
-                //status = STATUS.AROUND;
-                break;
-            case AWAY:
-                //time.reset();
-                //away();
-                break;
-            case TOBUILD:
-                toBuild();
-                break;
-            case RELEASEBLOCK:
-                release();
+            case AROUND:
+                around();
+                status = BUILDSTATUS.PARK;
                 break;
             case PARK:
-                //time.reset();
                 park();
+                status = BUILDSTATUS.STOP;
                 break;
             case STOP:
                 stop1();
@@ -81,3 +56,4 @@ public class RedTriangle extends BuildSuperOp {
         }
     }
 }
+
