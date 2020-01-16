b283a0a9be4e979eb43ef5a30fb5b70a932ff243
Alexa <alexa_becker@loomis.org>
2020-01-16 16:56:45 -0500

    added opmodes to park

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OpModes/RedTrianglePark.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.Autonomous.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Autonomous.BuildSuperOp;
+@Autonomous
+public class RedTrianglePark extends BuildSuperOp {
+    public BUILDSTATUS status = BUILDSTATUS.FLIPPER;
+    @Override
+    public void loop() {
+        startPointBuild = -1;
+        //declare telemetry for all motors/servos
+        //this allows us to see how the motors are behaving in the code
+        //and then compare it to how they perform in real life
+        telemetry.addData("Power", LatchMotor.getPower());
+        telemetry.addData("LatchMotor Position: ", LatchMotor.getCurrentPosition());
+        telemetry.addData("Front Right: ", FrontRightDrive.getCurrentPosition());
+        telemetry.addData("Back Left: ", BackLeftDrive.getCurrentPosition());
+        telemetry.addData("Back Right: ", BackRightDrive.getCurrentPosition());
+        telemetry.addData("Front Left: ", FrontLeftDrive.getCurrentPosition());
+        telemetry.addData("Status: ", status);
+        telemetry.addData("Latch Position: ", Latch.getPosition());
+
+        //switch statements for changing the status of the robot
+        //this allows us to use different code for each status
+        //there are methods created below the switch statement for easier reading
+        switch (status) {
+            case FLIPPER:
+                flipper();
+                status = BUILDSTATUS.TOFOUNDATION;
+                break;
+            case PARK:
+                park();
+                status = BUILDSTATUS.STOP;
+                break;
+            case STOP:
+                stop1();
+                break;
+        }
+    }
+}
