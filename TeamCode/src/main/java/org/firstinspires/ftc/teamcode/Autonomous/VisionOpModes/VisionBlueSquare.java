7a564655037bee53f9b3b327711a1579a5fa9d8a
Asher <ashkornfeld@gmail.com>
2020-02-11 21:05:27 -0500

    miscellaneous errors and fixes before testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/VisionOpModes/VisionBlueSquare.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/VisionOpModes/VisionBlueSquare.java
@@ -1,15 +1,10 @@
 package org.firstinspires.ftc.teamcode.Autonomous.VisionOpModes;
 
-import android.webkit.WebSettings;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Autonomous.PlayerSuperOp;
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.teamcode.Autonomous.CameraParams;
 
 @Autonomous
 public class VisionBlueSquare extends PlayerSuperOp {
@@ -25,18 +20,13 @@ public class VisionBlueSquare extends PlayerSuperOp {
     private static final String VUFORIA_KEY =
             "AUAq88//////AAABmU+bO6dpUU4BreRJC5efYI1U4Fc5EvLiP5eGiT94wpCspMiACoccxAAVAgEOcCw87pTuHz671RvMDs3dtUBYrJNGI/x/bm60AsIdy3J7prt5EP8xeJuiKjWX32EoIhEsRnqZPpQOmCh11Q5vboZhsCNkNGMNWUIufrVa2g4SKwkSAjaAdOla8w/LwPKbiQBYvwbikpCb01LQg8iVYzWJHBfWLbQcXbuEBQIG9VSgGzyz4RStzgfG5mCTO4UZQbs7P3b/oJIf2rSzd7Ng1HmpHjldX8uFnLMuvIjgG/mJENP/edAw51wRui/21dV8QNdhV8KwP+KBdgpyVBMj44+OlN4ZrGGRkxYDNzd7yptjiGfe";
 
-    public void init() {
-        CamType type = CamType.WEBCAM;
-        cvCamera = new CVCamera(type);
-        initCamera(cvCamera, type);
-    }
-
     @Override
     public void loop() {
         if (!ran) {
             time.reset();
             ran = !ran;
         }
+        //useful variables to help with testing
         telemetry.addData("position: ", "%f", cvCamera.blockPos);
         telemetry.addData("left: ", "%f", cvCamera.left);
         telemetry.addData("top: ", "%f", cvCamera.top);
@@ -46,6 +36,7 @@ public class VisionBlueSquare extends PlayerSuperOp {
         telemetry.addData("height: ", "%f", cvCamera.hh);
         switch (status) {
             case FLIPPER:
+                //method to locate skystone
                 cvCamera.findSkystone();
                 if (time.seconds() > 1) {
                     toBlock();
@@ -53,30 +44,41 @@ public class VisionBlueSquare extends PlayerSuperOp {
                 }
                 break;
             case TOBLOCK:
+                //once drive commands have been run and the queue is empty, align with skystone and grab it using the latch
                 if (accelDrive.isEmpty) {
                     if (cvCamera.blockPos < midpoint12) {
+                        //if skystone is the first stone, align in front of it
                         block = -1;
                         moveBackwards();
                         status = PLAYERSTATUS.AWAY2;
-                    } else if (cvCamera.blockPos < midpoint23) {
+                    }
+                    //if skystone is the second stone, grab skystone and move to build zone
+                    else if (cvCamera.blockPos < midpoint23) {
                         grab();
                         away();
                         status = PLAYERSTATUS.AWAY;
-                    } else if (cvCamera.blockPos < pos3 + (pos3 - midpoint23)) {
+                    }
+                    //if skystone is the third stone, align in front of it
+                    else if (cvCamera.blockPos < pos3 + (pos3 - midpoint23)) {
                         block = 1;
                         moveForwards();
                         status = PLAYERSTATUS.AWAY2;
-                    } else {
+                    }
+                    else {
+                        //grab skystone and move to build zone
                         telemetry.addLine("Block Position not in range, defaulting to middle block");
                         grab();
                         away();
                         status = PLAYERSTATUS.AWAY;
                     }
-                } else {
+                }
+                //cycle through drive commands
+                else {
                     updateAndDrive();
                 }
                 break;
             case AWAY2:
+                //if queue is empty, grab skystone and move to build zone
                 if (accelDrive.isEmpty) {
                     grab();
                     away();
@@ -86,6 +88,7 @@ public class VisionBlueSquare extends PlayerSuperOp {
                 }
                 break;
             case AWAY:
+                //release the skystone and park
                 if (accelDrive.isEmpty) {
                     release();
                     park();
@@ -94,6 +97,7 @@ public class VisionBlueSquare extends PlayerSuperOp {
                     updateAndDrive();
                 }
             case PARK:
+                //once parked, stop all motion
                 if (accelDrive.isEmpty) {
                     stop1();
                     status = PLAYERSTATUS.STOP;
